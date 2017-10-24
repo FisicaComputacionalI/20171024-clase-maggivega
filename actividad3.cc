8d36b4dd3e6db61d8ed0e2adba8e87eab34a745a
maggivega <31355214+maggivega@users.noreply.github.com>
2017-10-24 13:54:38 -0500

    Add files via upload

---
+++ actividad3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  long N;
+   long sum=0;
+   cout<<"dame un numero natural"<<endl;
+   cin>>N;
+   for(long i=1; i<=N; i++)
+     {sum=sum+1;
+	 }
+   cout<<"la suma es"<<sum<<endl;
+   return 0;
+  }
